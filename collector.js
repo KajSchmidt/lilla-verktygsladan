67270e796bd57d9b583402173104efccd62aa95d
Kaj Schmidt <kaj.schmidt@ntig.se>
2019-02-08 20:14:23 +0100

    Knappar och närvarande

--- collector.js
+++ collector.js
@@ -1,12 +1,29 @@
-(function() {
+function collectNames() {
 
   var studentList = []
 
 	$("#right>form>table.longlist>tbody").find('tr').slice(1, -1).each(function(){
-     studentList.push($(this).children('td').slice(1,2).text());
+
+        studentList.push($(this).children('td').slice(1,2).text());
 
   });
 
   console.log(studentList);
 
-})();
+}
+
+function collectAttendingNames() {
+
+  var studentList = []
+
+	$("#right>form>table.longlist>tbody").find('tr').slice(1, -1).each(function(){
+    var attendingState = $(this).children('td').slice(2,3).children('select').val();
+    if (attendingState == 0) {
+      studentList.push($(this).children('td').slice(1,2).text());
+    }
+
+  });
+
+  console.log(studentList);
+
+}
---
+++ injector.js
@@ -0,0 +1,6 @@
+(function() {
+
+  $("#right").prepend("<button onClick='collectNames()'>Samla namn</button>");
+  $("#right").prepend("<button onClick='collectAttendingNames()'>Samla närvarande namn</button>");
+
+})();

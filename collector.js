d28b124ee5c74a281b4943d241e341071c7c0d1d
Kaj Schmidt <kaj.schmidt@ntig.se>
2019-02-08 20:17:15 +0100

    Update collector.js

--- collector.js
+++ collector.js
@@ -17,8 +17,7 @@ function collectAttendingNames() {
   var studentList = []
 
 	$("#right>form>table.longlist>tbody").find('tr').slice(1, -1).each(function(){
-    var attendingState = $(this).children('td').slice(2,3).children('select').val();
-    if (attendingState == 0) {
+    if ($(this).children('td').slice(2,3).children('select').val()== 0) {
       studentList.push($(this).children('td').slice(1,2).text());
     }
 

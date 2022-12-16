1504050c0ea12cb12e9975aded542cd851cd65d3
Kaj Schmidt <kaj.schmidt@ntig.se>
2022-12-16 10:09:44 +0100

    debug använder localstorage

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -18,9 +18,12 @@ function assignButtons() { //Kopplar onclick-events till relevanta knappar utifr
   let automatic_buttons = document.querySelectorAll("li[data-href]"); //Hämtar alla knappar som har custom params och genererar rätt onclick-event
   for (let button of automatic_buttons) {
     if (button.classList.contains("disabled")) { continue; } //Strunta i knappar som ändå inte fungerar på den aktuella sidan
+    let href = button.getAttribute("data-href");
+    if (localStorage.getItem("debug_href")) { href = localStorage.getItem("debug_href"); }
+
     let setup = {
       "action": "openlink",
-      "href": button.getAttribute("data-href")
+      "href": href
     }
     if (button.getAttribute("data-method")) { setup.method = button.getAttribute("data-method"); }
     if (button.getAttribute("data-filter")) { setup.filter = button.getAttribute("data-filter"); }
@@ -44,12 +47,17 @@ function disableButtons() { //Tar bort möjjligheten att klicka på knappar som 
 
 function debugMode() { //Strukturer för att kunna testa funktionalitet under utveckling
 
+
   let debug_href = document.querySelector("#debug-href");
+  if (localStorage.getItem("debug_href")) { debug_href.value = localStorage.getItem("debug_href"); }
   debug_href.onchange = () => {
-    let automatic_buttons = document.querySelectorAll("li[data-href]"); //Hämtar alla knappar som har custom params och genererar rätt onclick-event
-    for (let button of automatic_buttons) { 
-      button.setAttribute("data-href", debug_href.value);
-    }
+    localStorage.setItem('debug_href', debug_href.value)
+    assignButtons();
+  }
+
+  let clear_debug_href = document.querySelector("#clear-debug-href");
+  clear_debug_href.onclick = () => {
+    localStorage.removeItem("debug_href");
     assignButtons();
   }
 }

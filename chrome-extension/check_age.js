ebb4d1483585afdcbe9538d4641427f8c1a06f85
Kaj Schmidt <kaj.schmidt@ntig.se>
2023-10-17 13:32:57 +0200

    Räknar ut ålder

--- chrome-extension/check_age.js
+++ chrome-extension/check_age.js
@@ -3,9 +3,10 @@
     let age_row_header = age_row.children[0];
     let age_row_age = age_row.children[1];
     if (age_row_header.innerText == "Personnummer") { //Dubbelkollar att det är rätt celler
-        let birthday = "20" + age_row_age.innerText.substring(0,6);
-        console.log(birthday);
-        birthday = birthday.substring(0,4) + "-" + birthday.substring(4,6) + "-" + birthday.substring(6,8)
-        console.log(birthday);
+        let birth_string = "20" + age_row_age.innerText.substring(0,2) + "-" + age_row_age.innerText.substring(2,4) + "-" + age_row_age.innerText.substring(4,6); //Formaterar YYYY-MM--DD
+        let birth_date = new Date(birth_string);
+        let age_diff = new Date(Date.now() - birth_date.getTime());  //Räknar ut skillnad i timestamp-format
+        let age = age_diff.getFullYear() - 1970; //Räknar om skillnaden till år
+        console.log(age);
     }
 })();
